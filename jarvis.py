6174491ff4babaa2fed921b837e8940b44a51977
devika <devikavarshney0003@gmail.com>
2022-12-27 23:00:24 +0530

    added listening property to jarvis

--- jarvis.py
+++ jarvis.py
@@ -1,5 +1,8 @@
 import datetime
+import speech_recognition as sr
 import pyttsx3
+import wikipedia
+
 engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
 # print(voices)
@@ -20,5 +23,26 @@ def wishMe():
     else:
         speak('Good evening')
     speak('I am jarvis, How can I help you?')
+
+
+def takeCommand():
+    # takes microphone input and returns output as text
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening..")
+        r.pause_threshold = 1
+        audio = r.listen(source)
+    try:
+        print("Recognizing..")
+        query = r.recognize_google(audio, language='en-in')
+        print(f"User said: {query}\n")
+    except Exception as e:
+        # print(e)
+        print("Say that again please..")
+        return "None"
+    return query
+
 if __name__ == "__main__":
     wishMe()
+    while true:
+        query = takeCommand().lower()
