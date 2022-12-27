5d758b40c9e21ed5c8af2ed4a79b0d646eb4e8d0
devika <devikavarshney0003@gmail.com>
2022-12-27 23:37:14 +0530

    added some command and actions that should be performed by jarvis when asked

--- jarvis.py
+++ jarvis.py
@@ -2,6 +2,8 @@ import datetime
 import speech_recognition as sr
 import pyttsx3
 import wikipedia
+import webbrowser
+import os
 
 engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
@@ -24,6 +26,8 @@ def wishMe():
         speak('Good evening')
     speak('I am jarvis, How can I help you?')
 
+def sendEmail(to,content):
+    
 
 def takeCommand():
     # takes microphone input and returns output as text
@@ -42,7 +46,46 @@ def takeCommand():
         return "None"
     return query
 
+
 if __name__ == "__main__":
     wishMe()
-    while true:
+    if 1:
         query = takeCommand().lower()
+
+        if 'wikipedia' in query:
+            speak('Searching Wikipedia..')
+            query = query.replace('Wikipedia', "")
+            results = wikipedia.summary(query, sentences=2)
+            speak("According to wikipedia")
+            print(results)
+            speak(results)
+        elif 'open youtube' in query:
+            webbrowser.open("youtube.com")
+
+        elif 'open google' in query:
+            webbrowser.open("google.com")
+
+        elif 'open stack overflow' in query:
+            webbrowser.open("stackoverflow.com")
+        elif 'play music' in query:
+            webbrowser.open('open.spotify.com')
+            # music_dir=""
+            # songs= os.listdir(music_dir)
+            # print(songs)
+            # os.startfile(os.path.join(music_dir,songs[0]))
+        elif "what is the time" in query:
+            strTime = datetime.datetime.now().strftime("%H:%H:%S")
+            # print("{strTime.hour}")
+            speak(f"The time is {strTime}")
+        elif "open code" in query:
+            codePath = "C:\\devu\\Microsoft VS Code\\Code.exe"
+            os.startfile(codePath)
+        elif 'email' in query:
+            try:
+                speak('What should I say?')
+                content = takeCommand()
+                to = "devikavarshney0003@gmail.com"
+                sendEmail(to, content)
+                speak('Email has been sent')
+            except Exception as e:
+                speak('Sorry email not sent')
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,4 @@
+
+            # songs= os.listdir(music_dir)
+            # print(songs)
+            # os.startfile(os.path
